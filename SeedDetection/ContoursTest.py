de825d83c3b0142a629ecc457ae324091e3ab2b8
Christian <christian.liin.hansen@gmail.com>
2015-03-27 12:31:10 +0100

    Close to have cropped an obb

--- SeedDetection/ContoursTest.py
+++ SeedDetection/ContoursTest.py
@@ -45,7 +45,7 @@ def getBoxPoints(rect):
     p4 = (box[3][0], box[3][1])
     return p1, p2, p3, p4
 
-def drawSproutBoundingBox(p1, p2, p3, p4, imgDraw):
+def drawBoundingBox(p1, p2, p3, p4, imgDraw):
         # Draw the oriente bouningbox
         lineWidth = 1
         boundingBoxColor = (0, 0, 255)
@@ -54,12 +54,20 @@ def drawSproutBoundingBox(p1, p2, p3, p4, imgDraw):
         cv2.line(imgDraw, p3, p4, boundingBoxColor, lineWidth)
         cv2.line(imgDraw, p4, p1, boundingBoxColor, lineWidth)
 
+def rotateImage(image, angle):
+  image_center = tuple(np.array(image.shape)/2)
+  rot_mat = cv2.getRotationMatrix2D(image_center,angle,1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape,flags=cv2.INTER_LINEAR)
+  return result
+
 def main():
 
     img = cv2.imread("/home/christian/Dropbox/E14/Master-thesis-doc/images/Improoseed_4_3_2015/images_with_15_cm_from_belt/trainingdata_with_par4/NGR/NGR_optimale.jpg", cv2.CV_LOAD_IMAGE_COLOR)
     imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(imgray,127,255,0)
+
     imgMorph = getClosing(3,thresh, 3, 3)
+    imgThreshold = imgMorph.copy()          # Copy the thresholded + morph mage befor findContours, since findContours somehow mess the input image.
     contours, hierarchy = cv2.findContours(imgMorph,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     print "A single contour looks like this", contours[0], contours[1]
@@ -69,7 +77,7 @@ def main():
     imgDraw = cv2.cvtColor(imgDraw,cv2.COLOR_GRAY2BGR)
 
     cv2.drawContours(imgDraw,contours,-1,(0,255,0),1)
-    cv2.imshow("Test of contour", imgDraw)
+    # cv2.imshow("Test of contour", imgDraw)
 
     # Draw a circle at the given pixel
     # x = contours[0][0][0][0]
@@ -77,12 +85,49 @@ def main():
     # cv2.circle(imgDraw, (x, y), 5, (255, 0, 0), -1)
     # cv2.imshow("Drawing a pixel", imgDraw)
 
+    # For each contour, calculate the minAreaRect and draw the boundingbox
+    listOfRects = []
     for contour in contours:
         rect = cv2.minAreaRect(contour)
+        listOfRects.append(rect)
         p1, p2, p3, p4 = getBoxPoints(rect)
-        drawSproutBoundingBox(p1, p2, p3, p4, imgDraw)
+        drawBoundingBox(p1, p2, p3, p4, imgDraw)
 
-    cv2.imshow("Drawing the boundingboxes", imgDraw)
+    # cv2.imshow("Drawing the boundingboxes", imgDraw)
+
+    # Now try to crop out a single boundinbox.
+    imgDrawCropped = imgDraw[200:400, 100:300]
+    cv2.imshow("Show the cropped imgDraw image", imgDrawCropped)
+
+    # Find out how much the boundingbox is rotated. The format is as followed [ ( (xCOM,yCOM), (widht, height), angle ) , .... , ]
+    # print "So the listOfRects is:", listOfRects
+
+    # The 3 contour has this information
+    print "The 4. contour has this information", listOfRects[3]
+    print "Mark this contour with a blue cirlce", listOfRects[3][0]
+
+    # Draw a circle at the given pixel
+    x = int(round(listOfRects[3][0][0],0))
+    y = int(round(listOfRects[3][0][1],0))
+
+    cv2.circle(imgDraw, (x, y), 5, (255, 0, 0), -1)
+    # cv2.imshow("Drawing a pixel again", imgDraw)
+
+    # Now get how much this contour is rotated
+    print "The rotation of this contour is:", listOfRects[3][2]
+    angleRotated = listOfRects[3][2]
+    # Now rotate the image
+
+    # Find out how much the angle should be rotated back, to be aligned as a nice rectangle
+    print "The contour has orientation of ths:", angleRotated
+
+    # In order to rotate a color image, we should split the colormap into three channles and rotate each channel.
+    # However this adds the cuputation time, so we just convert it to grayscale in order to rotate the color image onces.
+    imgDrawCroppedGray = cv2.cvtColor(imgDrawCropped,cv2.COLOR_BGR2GRAY)
+
+    # So rotating it +90 degree is a 90 degree counterclockwise direction.
+    rotatedImg = rotateImage(imgDrawCroppedGray, -30)
+    cv2.imshow("Show the rotated cropped image", rotatedImg)
 
     cv2.waitKey(0)
 
