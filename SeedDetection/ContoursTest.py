eab84238660e0cb90eb14e3771ed2ad2077fa7e1
Christian <christian.liin.hansen@gmail.com>
2015-03-31 23:14:20 +0200

    Now contourTest runs trough all pixels

--- SeedDetection/ContoursTest.py
+++ SeedDetection/ContoursTest.py
@@ -37,6 +37,23 @@ def getClosing(kernelSize, img_binary, iterations_erode, iterations_dilate):
         img_morph = cv2.copyMakeBorder(crop, kernelSize-1, 0, kernelSize-1, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
         return img_morph
 
+def getOpening(kernelSize, img_binary, iterations_erode, iterations_dilate):
+    kernel = np.ones((kernelSize, kernelSize), np.uint8)
+    img_erode = cv2.erode(img_binary, kernel, iterations=iterations_erode)
+    img_morph = cv2.dilate(img_erode, kernel, iterations=iterations_dilate)
+
+    # After the morph, the image will be shifter towards the origo with the
+    # size of the kernel.
+    # In order account for this, we do the following:
+    #   Crop the bottom and right side of the morphed image
+    #   equal to kernelsize - 1.
+    #   Append zeropadding to top and left side equal to kernelsize - 1.
+    height =  img_morph.shape[0]
+    width =  img_morph.shape[1]
+    crop = img_morph[0:height-kernelSize+1, 0:width-kernelSize+1]
+    img_morph = cv2.copyMakeBorder(crop, kernelSize-1, 0, kernelSize-1, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    return img_morph
+
 def getBoxPoints(rect):
     # http://stackoverflow.com/questions/18207181/opencv-python-draw-minarearect-rotatedrect-not-implemented
     box = cv2.cv.BoxPoints(rect)
@@ -67,22 +84,46 @@ def main():
     tc = TestClass()
     tc.testFunction()
 
+    # Read the RGB input image
     img = cv2.imread("/home/christian/Dropbox/E14/Master-thesis-doc/images/Improoseed_4_3_2015/images_with_15_cm_from_belt/trainingdata_with_par4/NGR/NGR_optimale.jpg", cv2.CV_LOAD_IMAGE_COLOR)
+    cv2.imshow("Input image", img)
+
+    # Convert RGB to grayscale
     imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(imgray,127,255,0)
 
-    # TestClass()
+    # Convert grayscale to binary
+    ret,thresh = cv2.threshold(imgray,128,128,0)
 
-    imgMorph = getClosing(3,thresh, 3, 3)
+    # Perform morph on binary image
+    imgMorph = getClosing(3, thresh, 3, 3)
     imgThreshold = imgMorph.copy()          # Copy the thresholded + morph mage befor findContours, since findContours somehow mess the input image.
+
+    # Convert the RGB input to HSV image
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Segment the HSV image by uing inRance.
+    lower_hsv = np.array([27, 0, 147], dtype=np.uint8)
+    upper_hsv = np.array([128, 255, 255], dtype=np.uint8)
+    imgSeg = cv2.inRange(imgHSV, lower_hsv, upper_hsv)
+    cv2.imshow("Test of HSV", imgSeg)
+
+    # Perform morph on HSV
+    imgMorphSeg = getClosing(3,imgSeg, 3, 3)
+    cv2.imshow("Test of HSV morphed closing", imgMorphSeg)
+
+    # Add the HSV together with the threshold image
+    addedImage = cv2.add(imgMorphSeg, imgThreshold)
+    cv2.imshow("Added image", addedImage)
+
+    # Find the contours in the binary morped image
     contours, hierarchy = cv2.findContours(imgMorph,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     print "A single contour looks like this", contours[0], contours[1]
-    print "The contours look like this:", contours
+    # print "The contours look like this:", contours
 
+    # Draw the contours
     imgDraw = imgMorph.copy()
     imgDraw = cv2.cvtColor(imgDraw,cv2.COLOR_GRAY2BGR)
-
     cv2.drawContours(imgDraw,contours,-1,(0,255,0),1)
     # cv2.imshow("Test of contour", imgDraw)
 
@@ -128,38 +169,63 @@ def main():
     # Try cropping out the bounding box, and not the oriented one.
 
     # Cropping is img[y1:y2, x1:x2]
-    imgBBcropped = imgThreshold[y1:y1+height1, x1:x1+width1]
+    imgBBcropped = addedImage[y1:y1+height1, x1:x1+width1]
     cv2.imshow("The croppped boundingbox", imgBBcropped)
+    cv2.imwrite("/home/christian/workspace_python/MasterThesis/SeedDetection/writefiles/imgBBcropped.jpg", imgBBcropped)
+
+
+    # Now try to run trough alll
+    print "The shape of the cropped boundingbox is", imgBBcropped.shape
+    print "The length is:", len(imgBBcropped)
+
+    sprout = []
+    seed = []
+    background = []
+
+    # The out for loop prints a several list, where each one has 61 elements.
+
+    # rowCounter = y1
+    # colCounter = x1
+
+    rowCounter = 0
+    colCounter = 0
+
+    for row in imgBBcropped:
+        for pixel in row:
+            if pixel == 255:
+                print "Hey we found a white pixel at this location: (", colCounter, ",", rowCounter, ")"
+            colCounter = colCounter + 1
+        rowCounter = rowCounter + 1
+        colCounter = 0
+        # colCounter = x1
+
+                # sprout.append(pixel)
+            # elif pixel == 128:
+                # seed.append(pixel)
+            # else:
+                # background.append(pixel)
 
     # Now the seed can be examinated.
-
-
-
-
     print "The x,y,width,height is", x1, y1, width1, height1
     cv2.circle(imgDraw, (x1, y1), 5, (255, 0, 0), -1)
-
-
-
-
     cv2.circle(imgDraw, (x, y), 5, (255, 0, 0), -1)
     cv2.imshow("Drawing a pixel again", imgDraw)
 
-    # Now get how much this contour is rotated
-    print "The rotation of this contour is:", listOfRects[3][2]
-    angleRotated = listOfRects[3][2]
-    # Now rotate the image
-
-    # Find out how much the angle should be rotated back, to be aligned as a nice rectangle
-    print "The contour has orientation of ths:", angleRotated
-
-    # In order to rotate a color image, we should split the colormap into three channles and rotate each channel.
-    # However this adds the cuputation time, so we just convert it to grayscale in order to rotate the color image onces.
-    imgDrawCroppedGray = cv2.cvtColor(imgDrawCropped,cv2.COLOR_BGR2GRAY)
+    # # Now get how much this contour is rotated
+    # print "The rotation of this contour is:", listOfRects[3][2]
+    # angleRotated = listOfRects[3][2]
+    # # Now rotate the image
+    #
+    # # Find out how much the angle should be rotated back, to be aligned as a nice rectangle
+    # print "The contour has orientation of ths:", angleRotated
+    #
+    # # In order to rotate a color image, we should split the colormap into three channles and rotate each channel.
+    # # However this adds the cuputation time, so we just convert it to grayscale in order to rotate the color image onces.
+    # imgDrawCroppedGray = cv2.cvtColor(imgDrawCropped,cv2.COLOR_BGR2GRAY)
 
     # # So rotating it +90 degree is a 90 degree counterclockwise direction.
-    rotatedImg = rotateImage(imgDrawCroppedGray, angleRotated+90)
-    cv2.imshow("Show the rotated cropped image", rotatedImg)
+    # rotatedImg = rotateImage(imgDrawCroppedGray, angleRotated+90)
+    # cv2.imshow("Show the rotated cropped image", rotatedImg)
 
     cv2.waitKey(0)
 
--- SeedDetection/SeedDetection.py
+++ SeedDetection/SeedDetection.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+
+
 """
 Created on 30/11-2014
 
